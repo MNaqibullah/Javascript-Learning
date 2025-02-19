4ee0a9414307b1398098645c0a836ffa2fc1a2fc
MNaqibullah <106157047+MNaqibullah@users.noreply.github.com>
2025-02-19 15:42:31 +0000

    Value Comparisson done

---
+++ 03_Comparisson.js
@@ -0,0 +1,19 @@
+let num1 = "3"
+
+let num2 = 3
+
+console.log(num1 == num2);
+
+console.log(num1 === num2);
+
+console.log(null >= 0);
+console.log(null == 0);
+console.log(null > 0);
+
+//== sign compare the value only if we pass string in number and compare with number it will auto convert the string to number.
+// === sign compare the data type and value both.
+
+
+
+
+
